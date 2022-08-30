99f463d1ff69eda71a106ec4dbfd7d3d82cebfd6
Fantox <shubhapratimbiswas@gmail.com>
2022-08-30 21:31:24 +0530

    Increased reply speed

--- Core.js
+++ Core.js
@@ -4992,31 +4992,6 @@ break
 
 default:
 
-/*
-    if (!isCmd && !isGroup){
-        const botreply = await axios.get(`http://api.brainshop.ai/get?bid=165801&key=1ftAuFL7Fhj21Fyp&uid=[uid]&msg=${budy}]`)
-       txt = `${botreply.data.cnt}`
-       m.reply(txt)
-
-
-
-await axios.get(`http://api.brainshop.ai/get?bid=165801&key=1ftAuFL7Fhj21Fyp&uid=[uid]&msg=${budy}]`)
-.then((response) => {
-        txt = `${response.data.cnt}`
-
-       m.reply(txt);http://api.brainshop.ai/get?bid=168758&key=Ci7eNhtxpxxDB5FQ&uid=[uid]&msg=[msg]
-  }
-
-*/
-
-  if (!isCmd && !m.isGroup){
-    const botreply = await axios.get(`http://api.brainshop.ai/get?bid=168758&key=Ci7eNhtxpxxDB5FQ&uid=[uid]&msg=[${budy}]`)
-    txt = `${botreply.data.cnt}`
-    m.reply(txt)
-    }
-
-
-
     
 if (budy.startsWith('=>')) {
 if (!isCreator) return reply(mess.botowner)
