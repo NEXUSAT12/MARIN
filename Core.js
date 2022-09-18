aef3bf31fae895db41589a0e018291790308d532
NEXUSAT12 <113586948+NEXUSAT12@users.noreply.github.com>
2022-09-18 19:07:01 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -924,11 +924,6 @@ let cron = require('node-cron')
     scheduled: true,
     timezone: "Asia/Kolkata"
     })
-	
-if (m.message) {
-            Miku.sendReadReceipt(m.chat, m.sender, [m.key.id])
-            console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
-        }
 
     if (tebaklagu.hasOwnProperty(m.sender.split('@')[0]) && isCmd) {
       kuis = true
