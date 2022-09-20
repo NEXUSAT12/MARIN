d361af678aaa2e324def78da696981d0421a7588
NEXUSAT12 <113586948+NEXUSAT12@users.noreply.github.com>
2022-09-20 11:41:28 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -5297,11 +5297,15 @@ case 'help': case 'h': case 'menu': case 'allmenu': case 'listmenu':{
  🔥 Type " *${prefix}help* " 𝐅𝐎𝐑 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐢𝐬𝐭.`
     
 
- let buttonshelpm = [
-	 { buttonId: `${prefix}support`,
-		      buttonText: { displayText :'❤️ support ❤️'}
-	  },{buttonId : `${prefix}owner`,
-	     buttonText:{'❤ OWNER ❤'}
+ let buttonshelpm = [{
+	 urlButton: {
+		 displayText: 'whatsapp❤️‍🔥',
+		 url: `${myweb}`
+		 }
+	 }, {quickReplyButton: {
+		 displayText: '👑Owner👑',
+		 id: `${prefix}owner`
+		 }
 	     }]
                 let buttonMessage = {
                     video:fs.readFileSync('./system/miku2.mp4'),gifPlayback:true,
