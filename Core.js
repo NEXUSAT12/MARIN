e34d87747a9e90ae5f69113b11d4e18c2f64ce9b
NEXUSAT12 <113586948+NEXUSAT12@users.noreply.github.com>
2022-09-20 10:59:56 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -5297,8 +5297,7 @@ case 'help': case 'h': case 'menu': case 'allmenu': case 'listmenu':{
  🔥 Type " *${prefix}help* " 𝐅𝐎𝐑 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐢𝐬𝐭.`
     
 
- let buttonshelpm = [{ buttonId: {
-                                   `${prefix}support`,
+ let buttonshelpm = [{ buttonId: { `${prefix}support`,
                                     buttonText: { displayText :'❤️ support ❤️'}
                                 }
                             }, {
